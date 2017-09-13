2e09ea9f7632d6ec5581778eff8348e3db9991fe
Jarod Towner <towja@icloud.com>
2017-09-13 17:12:03 +0930

    Added a dice roll feature which rolls #dS

--- cram.js
+++ cram.js
@@ -1,13 +1,42 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+var commandCharacter = '/';
+
+function getRandomInt (min, max) {
+  max++;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function rollDice (count, size) {
+  var rolls = [];
+  var total = 0;
+  for(var i = count; i > 0; i--) {
+    var roll = getRandomInt(1, size);
+    rolls.push(roll);
+    total += roll;
+  }
+  var rollSet = {
+    array: rolls,
+    total: total
+  };
+  return rollSet;
+}
+
 client.on('ready', () => {
-  console.log('I am ready!');
+  console.log('Powered On!');
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  var command = message.content.split(" ");
+  if (command[0] === 'roll' && command[1].indexOf('d') > 0) {
+    var diceRoll = command[1].split('d');
+    var count = +diceRoll[0];
+    var size = +diceRoll[1];
+    var roll = rollDice(count, size);
+    message.reply("You rolled " + roll.total + " " + JSON.stringify(roll.array));
   }
 });
 
