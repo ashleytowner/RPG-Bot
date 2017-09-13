5904c69c3af5050b1f1577d25c9b32387a4722ad
Jarod Towner <towja@icloud.com>
2017-09-13 17:39:53 +0930

    Changed roll command to require command character and added a getcommandinfo function

--- cram.js
+++ cram.js
@@ -3,6 +3,23 @@ const client = new Discord.Client();
 
 var commandCharacter = '/';
 
+var commands = [
+  {
+    name: "roll",
+    usage: "/roll [number]d[size]",
+    description: "Rolls [number] dice with [size] sides."
+  }
+];
+
+function getCommandInfo(cmdName) {
+  for (x in commands) {
+    if (commands[x].name === cmdName) {
+      return commands[x];
+    }
+  }
+  return null;
+}
+
 function getRandomInt (min, max) {
   max++;
   min = Math.ceil(min);
@@ -31,12 +48,20 @@ client.on('ready', () => {
 
 client.on('message', message => {
   var command = message.content.split(" ");
-  if (command[0] === 'roll' && command[1].indexOf('d') > 0) {
-    var diceRoll = command[1].split('d');
-    var count = +diceRoll[0];
-    var size = +diceRoll[1];
-    var roll = rollDice(count, size);
-    message.reply("You rolled " + roll.total + " " + JSON.stringify(roll.array));
+  if (command[0] === commandCharacter + 'roll') {
+    if (typeof(command[1]) !== 'undefined') {
+      if (command[1].indexOf('d') > 0) {
+        var diceRoll = command[1].split('d');
+        var count = +diceRoll[0];
+        var size = +diceRoll[1];
+        var roll = rollDice(count, size);
+        message.reply("You rolled " + roll.total + " " + JSON.stringify(roll.array));
+      } else {
+        message.reply("Usage: " + getCommandInfo("roll").usage);
+      }
+    } else {
+      message.reply("Usage: " + getCommandInfo("roll").usage);
+    }
   }
 });
 
