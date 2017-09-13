01530b3e0e30f49bae511adbe5227fea2e2865af
Jarod Towner <towja@icloud.com>
2017-09-13 23:13:40 +0930

    Added a newcharacter command and reformatted help

--- cram.js
+++ cram.js
@@ -15,11 +15,98 @@ var commands = [
     description: "Get an invite link to invite the bot to your server."
   },
   {
+    name: "newcharacter",
+    usage: commandCharacter + "newcharacter [name] [physical] [mental] [vitality] [luck] [skill1] [skill2] [skill3]",
+    description: "Creates a new character in the OnePageD20 system, created by Towja."
+  },
+  {
     name: "roll",
     usage:  commandCharacter + "roll [number]d[size]",
     description: "Rolls [number] dice with [size] sides."
   }
 ];
+
+function character(name, phy, men, vit, luc, skills) {
+  phy = +phy;
+  men = +men;
+  vit = +vit;
+  luc = +luc;
+  this.name = name;
+  this.physical = phy;
+  this.physUnmod = phy;
+  this.mental = men;
+  this.vitality = vit;
+  this.luck = luc;
+  this.weapons = [];
+  this.maxHp = 10 + vit;
+  this.hp = this.maxHp;
+  this.defense = 10 + vit;
+  this.addWeapons = function (name, type, bonus, damage) {
+    this.weapons.push({
+      "name":name,
+      "type":type,
+      "bonus":bonus,
+      "damage":damage
+    });
+    return this;
+  }
+  this.armor = {};
+  this.setArmor = function (name, defense, penalty) {
+    this.defense = 10 + this.vitality + defense;
+    this.physical = this.physUnmod + penalty;
+    this.armor = {
+      "name":name,
+      "defense":defense,
+      "penalty":penalty
+    };
+  }
+  // skills
+  this.athletics = phy;
+  this.lore = men;
+  this.martial = phy;
+  this.medicine = men;
+  this.rhetoric = Math.floor((men + luc) / 2);
+  this.science = men;
+  this.subterfuge = Math.floor((men + luc) / 2);
+  this.survival = Math.floor((men + phy) / 2);
+  for (x in skills) {
+    switch (skills[x]) {
+      case "athletics":
+        this.athletics++;
+        break;
+      case "lore":
+        this.lore++;
+        break;
+      case "martial":
+        this.martial++;
+        break;
+      case "medicine":
+        this.medicine++;
+        break;
+      case "rhetoric":
+        this.rhetoric++;
+        break;
+      case "science":
+        this.science++;
+        break;
+      case "subterfuge":
+        this.subterfuge++;
+        break;
+      case "survival":
+        this.survival++;
+        break;
+      default:
+        break;
+    }
+  }
+  this.toString = function() {
+    return "Name: " + this.name + "\nPHY: " + this.physical + "   MEN: " + this.mental + "\nVIT: " + this.vitality +
+    "   LUC: " + this.luck + "\nHP: " + this.hp + "/" + this.maxHp + "\nDefense: " + this.defense + "\nSkills:\n- Athletics: " + this.athletics + "\n- Lore: " + this.lore + "\n- Martial: " + this.martial +
+    "\n- Medicine: " + this.medicine + "\n- Rhetoric: " + this.rhetoric + "\n- Science: " + this.science + "\n- subterfuge: " + this.subterfuge +
+    "\n- Survival: " + this.survival;
+  }
+  return this;
+}
 
 function getCommandInfo(cmdName) {
   for (x in commands) {
@@ -61,17 +148,22 @@ client.on('message', message => {
   if (command[0] === commandCharacter + 'help') {
     if (getCommandInfo(command[1]) != null) {
       if (getCommandInfo(command[1]) != null) {
-        message.author.send("Usage: " + getCommandInfo(command[1]).usage);
+        message.reply("Usage: " + getCommandInfo(command[1]).usage + "\n\n" + getCommandInfo(command[1]).description, {code: true});
       }
     } else {
-      var helpString = "I am a bot designed to make it easier to play RPGs over Discord. I was created by Towja.\n";
+      var helpString = "I am a bot designed to make it easier to play RPGs over Discord. I was created by Towja.\n\n";
       for (x in commands) {
-        helpString += commands[x].name + ": " + commands[x].description + "\n";
+        helpString += "- " + commands[x].name + ": " + commands[x].description + "\n";
       }
       message.author.send(helpString, {code: true});
     }
   } else if (command[0] === commandCharacter + 'invite') {
     message.reply("Here is my invite link: \nhttps://discordapp.com/oauth2/authorize?client_id=203440659728826368&scope=bot&permissions=67230720");
+  } else if (command[0] === commandCharacter + 'newcharacter') {
+    if (command[1] != null && command[2] != null && command[3] != null && command[4] != null && command[5] != null && command[6] != null && command[7] != null && command[8] != null) {
+      var char = character(command[1], command[2], command[3], command[4], command[5], [command[6], command[7], command[8]]);
+      message.reply(char.toString(), {code: true});
+    }
   } else if (command[0] === commandCharacter + 'roll') {
     if (typeof(command[1]) !== 'undefined') {
       if (command[1].indexOf('d') > 0) {
@@ -81,10 +173,10 @@ client.on('message', message => {
         var roll = rollDice(count, size);
         message.reply("You rolled " + roll.total + " " + JSON.stringify(roll.array));
       } else {
-        message.reply("Usage: " + getCommandInfo("roll").usage);
+        message.reply("Usage: " + getCommandInfo("roll").usage, {code: true});
       }
     } else {
-      message.reply("Usage: " + getCommandInfo("roll").usage);
+      message.reply("Usage: " + getCommandInfo("roll").usage, {code: true});
     }
   }
 });
