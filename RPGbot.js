49e47ba8a788da39c54069c875f219879972f129
Jarod Towner <jarod.towner@gmail.com>
2017-10-09 19:16:52 +1030

    added new name thing and formatting boolean

--- RPGbot.js
+++ RPGbot.js
@@ -169,10 +169,18 @@ client.on('message', message => {
       }
       var result = "";
       for (i = amount; i > 0; i--) {
-        //result += ((amount - i) + 1) + ". ";
+        var format = false;
         for (x in table) {
-          var index = getRandomInt(0, table[x].length - 1);
-          result += x + ": " + table[x][index] + "\t"
+          if (x != "format") {
+            var index = getRandomInt(0, table[x].length - 1);
+            if (format) {
+              result += x + ": " + table[x][index] + "\t";
+            } else {
+              result += table[x][index];
+            }
+          } else {
+            format = table[x];
+          }
         }
         result += "\n\n";
       }
